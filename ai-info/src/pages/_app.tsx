76b64060ee1792446dcb35f8698cd6f2bb865508
tsanghans626 <tsanghans626@gmail.com>
2025-09-10 11:11:16 +0800

    Integrated t3 stack

---
+++ ai-info/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
+import "../styles/global.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default trpc.withTRPC(MyApp);
